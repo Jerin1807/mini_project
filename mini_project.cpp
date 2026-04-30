dc4ebe10de6bb0503bcc563e9fa3d85287a248d6
Jerin1807 <jj7160@srmist.edu.in>
2026-04-30 22:10:39 +0530

    Enhanced delete functionality to handle duplicate entries

--- mini_project.cpp
+++ mini_project.cpp
@@ -72,16 +72,20 @@ int main() {
             string deleteName;
             cout << "Enter name to delete: ";
             cin >> deleteName;
-
             bool found = false;
-            for (int i = 0; i < contacts.size(); i++) {
-                if (contacts[i].name == deleteName) {
-                    contacts.erase(contacts.begin() + i);
-                    cout << "Contact Deleted!\n";
-                    found = true;
-                    break;
-                }
-            }
+
+        for (int i = 0; i < contacts.size(); i++) {
+           if (contacts[i].name == deleteName) {
+              contacts.erase(contacts.begin() + i);
+              i--; // adjust index after deletion
+              found = true;
+    }
+}
+
+       if (found) {
+          cout << "All matching contacts deleted!\n";
+}
+
 
             if (!found) {
                 cout << "Contact not found.\n";
